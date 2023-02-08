ec3925bbfb0cbe750251f69eadc48e95a1e75b80
semunve <124305081+semunve@users.noreply.github.com>
2023-02-08 13:22:29 +0100

    Update Calculadora.java

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package calculadora;
 
 import java.awt.BorderLayout;
@@ -12,15 +9,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- *
- * @author sebas
- */
+
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
